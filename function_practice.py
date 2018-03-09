ddf4d51ab06bdf48582b6cebba650ac2ade50cd7
airraidsiren1974 <33739256+airraidsiren1974@users.noreply.github.com>
2018-03-08 20:30:51 -0800

    Update function_practice.py

--- function_practice.py
+++ function_practice.py
@@ -1,5 +1,15 @@
 ###Assorted Function Practice
 
 def square(value):
+    """Returns the square of a value"""
     new_value = value ** 2
     print(new_value)
+    
+    
+### Next Function####
+
+def raise_to_power(value1, value2):
+    """Raise the value1 to the power of value2"""
+    new_value = value1 ** value2
+    return new_value
+    
